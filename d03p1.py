e627ad700ab6582f61e68543c8e519355601b8d8
Alister McKinley <alpatine@gmail.com>
2023-12-11 01:21:36 +1100

    Refactor day 3

--- d03p1.py
+++ d03p1.py
@@ -12,22 +12,22 @@ raw_data = '''467..114..
 with open('d03data.txt') as file:
     raw_data = file.read()
 
-# convert the block of text into a character matrix
-matrix = [[char for char in line] for line in raw_data.splitlines()]
-height = len(matrix)
-width = len(matrix[0])
+# convert the block of text into a character grid
+grid = [[char for char in line] for line in raw_data.splitlines()]
+height = len(grid)
+width = len(grid[0])
 
 row = 0
 number_sum = 0
 while row < height:
     col = 0
     while col < width:
-        char = matrix[row][col]
+        char = grid[row][col]
         if char.isdigit():
             # start of a number, find the end
             stop_col = col + 1
             while stop_col < width:
-                if not matrix[row][stop_col].isdigit():
+                if not grid[row][stop_col].isdigit():
                     break
                 stop_col += 1
             
@@ -44,7 +44,7 @@ while row < height:
                 rows_to_check.append(row+1)
             for check_row in rows_to_check:
                 for check_col in range(max(0, col - 1), min(stop_col + 1, width)):
-                    check_char = matrix[check_row][check_col]
+                    check_char = grid[check_row][check_col]
                     if is_symbol(check_char):
                         include_number = True
                         break
@@ -56,13 +56,13 @@ while row < height:
             if stop_col < width:
                 cols_to_check.append(stop_col)
             for check_col in cols_to_check:
-                check_char = matrix[row][check_col]
+                check_char = grid[row][check_col]
                 if is_symbol(check_char):
                     include_number = True
             
             # determine the number and add to sum
             if include_number:
-                number_chars = matrix[row][col:stop_col]
+                number_chars = grid[row][col:stop_col]
                 number_string = ''.join(number_chars)
                 number_sum += int(number_string)
             col = stop_col
--- d03p2.py
+++ d03p2.py
@@ -15,38 +15,38 @@ raw_data = '''467..114..
 with open('d03data.txt') as file:
     raw_data = file.read()
 
-# convert the block of text into a character matrix
-matrix = [[char for char in line] for line in raw_data.splitlines()]
-height = len(matrix)
-width = len(matrix[0])
+def extract_number(grid, row, col):
+    width = len(grid[0])
+    start_col = col
+    stop_col = start_col + 1
+    while start_col >= 0 and grid[row][start_col - 1].isdigit():
+        start_col -= 1
+    while stop_col < width and grid[row][stop_col].isdigit():
+        stop_col += 1
+    return int(''.join(grid[row][start_col:stop_col]))
+
+# convert the block of text into a character grid
+grid = [[char for char in line] for line in raw_data.splitlines()]
+height = len(grid)
+width = len(grid[0])
 
 row = 0
 gear_ratio_sum = 0
 while row < height:
     col = 0
     while col < width:
-        char = matrix[row][col]
+        char = grid[row][col]
         if char == '*':
             gear_ratio_factors = []
             # Could be a gear - need to detect numbers around it
 
             # check if there is a number to the left
-            if col > 0 and matrix[row][col - 1].isdigit():
-                start_col = col - 1
-                stop_col = start_col + 1
-                while start_col >= 0 and matrix[row][start_col - 1].isdigit():
-                    start_col -= 1
-                factor = int(''.join(matrix[row][start_col:stop_col]))
-                gear_ratio_factors.append(factor)
+            if col > 0 and grid[row][col - 1].isdigit():
+                gear_ratio_factors.append(extract_number(grid, row, col - 1))
             
             # check if there is a number to the right
-            if col + 1 < width and matrix[row][col + 1].isdigit():
-                start_col = col + 1
-                stop_col = start_col + 1
-                while stop_col < width and matrix[row][stop_col].isdigit():
-                    stop_col += 1
-                factor = int(''.join(matrix[row][start_col:stop_col]))
-                gear_ratio_factors.append(factor)
+            if col + 1 < width and grid[row][col + 1].isdigit():
+                gear_ratio_factors.append(extract_number(grid, row, col + 1))
             
             # check for numbers above and below
             rows_to_check = []
@@ -55,34 +55,16 @@ while row < height:
             if row + 1 < height:
                 rows_to_check.append(row + 1)
             for check_row in rows_to_check:
-                if matrix[check_row][col].isdigit():
-                    # if the char directly above/below id a digit, there can only be one number
-                    start_col = col
-                    stop_col = col + 1
-                    # determine how far left this goes
-                    while start_col >= 0 and matrix[check_row][start_col - 1].isdigit():
-                        start_col -= 1
-                    while stop_col < width and matrix[check_row][stop_col].isdigit():
-                        stop_col += 1
-                    factor = int(''.join(matrix[check_row][start_col:stop_col]))
-                    gear_ratio_factors.append(factor)
+                if grid[check_row][col].isdigit():
+                    # if the char directly above/below is a digit, there can only be one number
+                    gear_ratio_factors.append(extract_number(grid, check_row, col))
                 else:
                     # need to check to the left and right on the row, could be two numbers
-                    if col > 0 and matrix[check_row][col - 1].isdigit():
-                        start_col = col - 1
-                        stop_col = start_col + 1
-                        while start_col >= 0 and matrix[check_row][start_col - 1].isdigit():
-                            start_col -= 1
-                        factor = int(''.join(matrix[check_row][start_col:stop_col]))
-                        gear_ratio_factors.append(factor)
+                    if col > 0 and grid[check_row][col - 1].isdigit():
+                        gear_ratio_factors.append(extract_number(grid, check_row, col - 1))
                     
-                    if col + 1 < width and matrix[check_row][col + 1].isdigit():
-                        start_col = col + 1
-                        stop_col = start_col + 1
-                        while stop_col < width and matrix[check_row][stop_col].isdigit():
-                            stop_col += 1
-                        factor = int(''.join(matrix[check_row][start_col:stop_col]))
-                        gear_ratio_factors.append(factor)
+                    if col + 1 < width and grid[check_row][col + 1].isdigit():
+                        gear_ratio_factors.append(extract_number(grid, check_row, col + 1))
 
             if len(gear_ratio_factors) == 2:
                 gear_ratio = prod(gear_ratio_factors)
