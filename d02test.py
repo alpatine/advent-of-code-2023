6fb3dcc20f262c28618f8b0c4d5efbabf2095063
Alister McKinley <alpatine@gmail.com>
2023-12-28 09:47:38 +1100

    FIx typo in day 2 tests

--- d02test.py
+++ d02test.py
@@ -14,7 +14,7 @@ Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green'''
     def test_part1_solution(self):
         self.assertEqual(d02p1(), 2256)
 
-    def test_part2_exampple1(self):
+    def test_part2_example1(self):
         raw_data = '''Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
 Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
 Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
