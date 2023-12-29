472f60801fa7d6599e9aebf866d8b25ea3bf73b0
Alister McKinley <alpatine@gmail.com>
2023-12-29 13:53:13 +1100

    Solve day 9 part 2

--- d09test.py
+++ d09test.py
@@ -1,6 +1,7 @@
 from unittest import TestCase
 
 from d09p1 import d09p1
+from d09p2 import d09p2
 
 
 class Day09_Test(TestCase):
@@ -12,3 +13,12 @@ class Day09_Test(TestCase):
     
     def test_part1_solution(self):
         self.assertEqual(d09p1(), 1731106378)
+
+    def test_part2_example1(self):
+        raw_data = '''0 3 6 9 12 15
+1 3 6 10 15 21
+10 13 16 21 30 45'''
+        self.assertEqual(d09p2(raw_data), 2)
+    
+    def test_part2_solution(self):
+        self.assertEqual(d09p2(), 1087)
