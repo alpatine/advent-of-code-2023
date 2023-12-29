94e5930fb013112b4e038452aaa997326093abf6
Alister McKinley <alpatine@gmail.com>
2023-12-29 13:43:02 +1100

    Solve day 9 part 1

---
+++ d09p1.py
@@ -0,0 +1,33 @@
+from itertools import pairwise
+
+
+def predict_next_value(sequence):
+    def is_zero(n): return n == 0
+    stack = []
+    while not all(map(is_zero, sequence)):
+        stack.append(sequence)
+        new_sequence = []
+        for left,right in pairwise(sequence):
+            new_sequence.append(right - left)
+        sequence = new_sequence
+    
+    total = 0
+    for sequence in reversed(stack):
+        total += sequence[-1]
+    return total
+
+def d09p1(raw_data = None):
+    if raw_data is None:
+        with open('d09data.txt') as file:
+            raw_data = file.read()
+    
+    next_value_sum = 0
+    for line in raw_data.splitlines():
+        sequence = [int(s) for s in line.split()]
+        next_value = predict_next_value(sequence)
+        next_value_sum += next_value
+
+    return next_value_sum
+
+if __name__ == '__main__':
+    print(d09p1())
---
+++ d09test.py
@@ -0,0 +1,14 @@
+from unittest import TestCase
+
+from d09p1 import d09p1
+
+
+class Day09_Test(TestCase):
+    def test_part1_example1(self):
+        raw_data = '''0 3 6 9 12 15
+1 3 6 10 15 21
+10 13 16 21 30 45'''
+        self.assertEqual(d09p1(raw_data), 114)
+    
+    def test_part1_solution(self):
+        self.assertEqual(d09p1(), 1731106378)
